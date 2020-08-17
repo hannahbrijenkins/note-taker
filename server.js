31cea6a45006fbb093edc69bad5f8785937d0229
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-08-16 20:31:32 -0400

    added heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+// create port, import express
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+// activates app
+app.listen(PORT, () => {
+    console.log('Your server is now active!')
+})
